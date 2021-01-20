0ee178afd45af75e3a1f71ed4aaf2677429bbdbc
Pahud Hsieh <pahudnet@gmail.com>
2021-01-20 20:33:38 +0800

    feat: use cdk-ec2spot to generate AutoScaling Group (#9)
    Fixes #8

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as autoscaling from '@aws-cdk/aws-autoscaling';
 import * as ec2 from '@aws-cdk/aws-ec2';
-import * as iam from '@aws-cdk/aws-iam';
 import * as cdk from '@aws-cdk/core';
+import * as ec2spot from 'cdk-ec2spot';
 
 /**
  * Construct properties for the EKS-D cluster
@@ -52,55 +52,6 @@ export enum SpotInstanceType {
   PERSISTENT = 'persistent',
 }
 
-export enum InstanceInterruptionBehavior {
-  HIBERNATE = 'hibernate',
-  STOP = 'stop',
-  TERMINATE = 'terminate',
-}
-
-export enum BlockDurationMinutes {
-  ONE_HOUR = 60,
-  TWO_HOURS = 120,
-  THREE_HOURS = 180,
-  FOUR_HOURS = 240,
-  FIVE_HOURS = 300,
-  SIX_HOURS = 360,
-}
-
-export interface SpotOptions {
-  readonly spotInstanceType?: SpotInstanceType;
-  readonly validUntil?: string;
-  readonly maxPrice?: string;
-  readonly instanceInterruptionBehavior?: InstanceInterruptionBehavior;
-  readonly blockDurationMinutes?: BlockDurationMinutes;
-
-}
-
-export interface LaunchTemplateOptions {
-  readonly machineImage: ec2.IMachineImage;
-  readonly instanceType: ec2.InstanceType;
-  readonly spotOptions?: SpotOptions;
-  readonly userData: ec2.UserData;
-  readonly instanceProfile?: iam.CfnInstanceProfile;
-}
-
-export interface AutoScalingGroupOptions {
-  /**
-   * The vpc for the AutoScalingGroup
-   */
-  readonly vpc: ec2.IVpc;
-  /**
-   * Launch template options for the AutoScalingGroup
-   */
-  readonly launchTemplate: LaunchTemplateOptions;
-  /**
-   * default capacity size for the Auto Scaling Group
-   *
-   * @default 1
-   */
-  readonly defaultCapacitySize?: number;
-}
-
 /**
  * Represents the EKS-D cluster
  */
@@ -119,57 +70,22 @@ export class Cluster extends cdk.Construct {
     // create auto scaling group with launch template
     this._createAutoScalingGroup('ASG', {
       vpc,
-      launchTemplate: {
-        instanceType: props.defaultInstanceType ?? this.defaultInstanceType,
-        machineImage: props.machineImage ?? new UbumtuAmiProvider(this, userData).amiId,
-        spotOptions: props.spot ? {
-          spotInstanceType: SpotInstanceType.ONE_TIME,
-        } : undefined,
-        userData,
-      },
+      defaultCapacitySize: props.capacitySize,
+      instanceType: props.defaultInstanceType ?? this.defaultInstanceType,
+      machineImage: props.machineImage ?? new UbumtuAmiProvider(this, userData).amiId,
+      userData,
+      spotOptions: props.spot ? {
+        spotInstanceType: SpotInstanceType.ONE_TIME,
+      } : undefined,
     });
 
     if (props.outputAmiId !== false) {
       new cdk.CfnOutput(this, 'AmiId', { value: new UbumtuAmiProvider(this, userData).amiId.getImage(this).imageId });
     }
   }
-  private _createAutoScalingGroup(id: string, options: AutoScalingGroupOptions): autoscaling.AutoScalingGroup {
-    const asg = new autoscaling.AutoScalingGroup(this, id, {
-      instanceType: this.defaultInstanceType,
-      machineImage: new ec2.AmazonLinuxImage(),
-      vpc: options.vpc,
-      minCapacity: options.defaultCapacitySize ?? 1,
-    });
-    const cfnInstanceProfile = asg.node.tryFindChild('InstanceProfile') as iam.CfnInstanceProfile;
-    asg.role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
-    const ops = options.launchTemplate;
-    const lt = new ec2.CfnLaunchTemplate(this, `LaunchTemplate${id}`, {
-      launchTemplateData: {
-        imageId: ops.machineImage.getImage(this).imageId,
-        instanceType: ops.instanceType.toString(),
-        userData: cdk.Fn.base64(ops.userData.render()),
-        instanceMarketOptions: {
-          marketType: ops.spotOptions ? 'spot': undefined,
-          spotOptions: {
-            blockDurationMinutes: ops.spotOptions?.blockDurationMinutes?.valueOf(),
-            instanceInterruptionBehavior: ops.spotOptions?.instanceInterruptionBehavior?.valueOf(),
-            maxPrice: ops.spotOptions?.maxPrice,
-            spotInstanceType: ops.spotOptions?.spotInstanceType?.valueOf(),
-          },
-        },
-        iamInstanceProfile: {
-          arn: cfnInstanceProfile.attrArn,
-        },
-      },
-    });
-    // property override
-    const cfnAsg = asg.node.tryFindChild('ASG') as autoscaling.CfnAutoScalingGroup;
-    cfnAsg.addPropertyDeletionOverride('LaunchConfigurationName');
-    cfnAsg.addPropertyOverride('LaunchTemplate', {
-      LaunchTemplateId: lt.ref,
-      Version: lt.attrLatestVersionNumber,
-    });
-
+  private _createAutoScalingGroup(id: string, options: ec2spot.AutoScalingGroupOptions): autoscaling.AutoScalingGroup {
+    const provider = new ec2spot.Provider(this, 'ASGProvider');
+    const asg = provider.createAutoScalingGroup(id, options);
     return asg;
   }
 }
