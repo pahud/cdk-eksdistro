f1005f1a3a4797ce449822626062c70490c04d8c
Pahud <pahudnet@gmail.com>
2021-06-07 18:21:24 +0800

    bump versions

--- .projenrc.js
+++ .projenrc.js
@@ -1,4 +1,4 @@
-const { AwsCdkConstructLibrary } = require('projen');
+const { AwsCdkConstructLibrary, DependenciesUpgradeMechanism } = require('projen');
 
 const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
 
@@ -21,9 +21,17 @@ const project = new AwsCdkConstructLibrary({
   peerDeps: [
     'cdk-ec2spot',
   ],
-  dependabot: false,
-  releaseBranches: ['main'],
-  defaultReleaseBranch: ['main'],
+  depsUpgrade: DependenciesUpgradeMechanism.githubWorkflow({
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      secret: AUTOMATION_TOKEN,
+    },
+  }),
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['pahud'],
+  },
+  defaultReleaseBranch: 'main',
   publishToPypi: {
     distName: 'cdk-eksdistro',
     module: 'cdk_eksdistro',
@@ -41,46 +49,6 @@ const project = new AwsCdkConstructLibrary({
     'kubernetes',
     'k8s',
   ],
-});
-
-// create a custom projen and yarn upgrade workflow
-const workflow = project.github.addWorkflow('ProjenYarnUpgrade');
-
-workflow.on({
-  schedule: [{
-    cron: '11 0 * * *',
-  }], // 0:11am every day
-  workflow_dispatch: {}, // allow manual triggering
-});
-
-workflow.addJobs({
-  upgrade: {
-    'runs-on': 'ubuntu-latest',
-    'steps': [
-      { uses: 'actions/checkout@v2' },
-      {
-        uses: 'actions/setup-node@v1',
-        with: {
-          'node-version': '10.17.0',
-        },
-      },
-      { run: 'yarn upgrade' },
-      { run: 'yarn projen:upgrade' },
-      // submit a PR
-      {
-        name: 'Create Pull Request',
-        uses: 'peter-evans/create-pull-request@v3',
-        with: {
-          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
-          'commit-message': 'chore: upgrade projen',
-          'branch': 'auto/projen-upgrade',
-          'title': 'chore: upgrade projen and yarn',
-          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
-          'labels': 'auto-merge',
-        },
-      },
-    ],
-  },
 });
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'yarn-error.log'];
